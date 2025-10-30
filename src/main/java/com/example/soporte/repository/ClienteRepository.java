bc9ac03cbaa0f9a2b8f1f908fac485620fba4102
José Dueñas <110559432+TheGuard2712@users.noreply.github.com>
2025-10-30 08:28:14 -0500

    actualización de separación de texto

--- src/main/java/com/example/soporte/repository/ClienteRepository.java
+++ src/main/java/com/example/soporte/repository/ClienteRepository.java
@@ -1,1 +1,5 @@
-package com.example.soporte.repository; import com.example.soporte.model.Cliente; import org.springframework.data.jpa.repository.JpaRepository; public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
+package com.example.soporte.repository; 
+import com.example.soporte.model.Cliente; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
